b39ee526ceb9ad8123e167b11f63fd2e210ec2fa
kh1200 <67504760+kh1200@users.noreply.github.com>
2020-06-30 20:34:28 +0600

    new website

--- scripts/form-u6093.php
+++ scripts/form-u6093.php
@@ -30,9 +30,6 @@ $form = array(
 		'from' => 'khashik1200@gmail.com',
 		'to' => 'khashik1200@gmail.com'
 	),
-	'recaptcha2' => array(
-		'private_key' => '8482'
-	),
 	'fields' => array(
 		'custom_U6095' => array(
 			'order' => 1,
@@ -59,16 +56,6 @@ $form = array(
 			'label' => 'Message',
 			'required' => false,
 			'errors' => array(
-			)
-		),
-		'g-recaptcha-response' => array(
-			'order' => 4,
-			'type' => 'recaptcha2',
-			'label' => 'Image Verification',
-			'required' => true,
-			'errors' => array(
-				'required' => 'Field \'Image Verification\' is required.',
-				'format' => 'Incorrect reCAPTCHA 2.0 value.'
 			)
 		)
 	)
